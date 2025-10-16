f2429ff819e66eee0edab81ece72a8575cc3a6e9
xuanphat06071997 <111958194+xuanphat11112003@users.noreply.github.com>
2025-10-16 22:11:36 +0700

    Fix image format comments in scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -119,8 +119,7 @@ function getHeartImagesFromURL() {
     './img/heart2.jpg',  // Chuyển từ JPG sang PNG
     './img/heart5.svg',  // SVG đã nhẹ
     './img/heart3.jpg',  // Chuyển từ JPG sang PNG
-    './img/heart4.jpg',   // Chuyển từ JPG sang PNG
-    './img/IMG_0006.jpeg'
+    './img/heart4.jpg'   // Chuyển từ JPG sang PNG
   ] ;
   
   console.log("Không có URL, sử dụng ảnh default:", defaultImages.length, "ảnh");
